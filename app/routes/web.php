1b239ebe22fdf6a9f60a100ea404549ac38f29ff
agungharyanto <agung.haryantoo@gmail.com>
2026-08-02 14:41:42 +0000

    v0.2.0 wire customer web routes; land authenticated users on customer list

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Livewire\Customers\CustomerIndex;
+use App\Livewire\Customers\CustomerShow;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware('auth')->name('web.')->group(function () {
+    Route::get('/customers', CustomerIndex::class)->name('customers.index');
+    Route::get('/customers/{customer}', CustomerShow::class)->name('customers.show');
 });

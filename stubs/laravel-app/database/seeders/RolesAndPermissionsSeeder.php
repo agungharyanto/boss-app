b89cab9dcb55deb4f2c15a176d162dd9b7199401
agungharyanto <agung.haryantoo@gmail.com>
2026-08-02 14:05:06 +0000

    v0.2.0 seed customer CRM permissions (customers.view/manage, customer_contacts.manage, customer_timeline.view)

--- stubs/laravel-app/database/seeders/RolesAndPermissionsSeeder.php
+++ stubs/laravel-app/database/seeders/RolesAndPermissionsSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
 class RolesAndPermissionsSeeder extends Seeder
@@ -28,5 +29,37 @@ class RolesAndPermissionsSeeder extends Seeder
         foreach ($roles as $role) {
             Role::firstOrCreate(['name' => $role, 'guard_name' => 'web']);
         }
+
+        $this->seedCustomerCrmPermissions($roles);
+    }
+
+    /**
+     * Permission modul Customer CRM (v0.2.0). Semua role bisa melihat data
+     * pelanggan dan timeline (butuh untuk kerja masing-masing), tapi hanya
+     * customer_service dan super_admin yang bisa mengubah data pelanggan
+     * dan kontak keluarga.
+     */
+    private function seedCustomerCrmPermissions(array $allRoles): void
+    {
+        $viewPermissions = [
+            'customers.view',
+            'customer_timeline.view',
+        ];
+
+        $managePermissions = [
+            'customers.manage',
+            'customer_contacts.manage',
+        ];
+
+        foreach ([...$viewPermissions, ...$managePermissions] as $permission) {
+            Permission::firstOrCreate(['name' => $permission, 'guard_name' => 'web']);
+        }
+
+        foreach ($allRoles as $role) {
+            Role::findByName($role, 'web')->givePermissionTo($viewPermissions);
+        }
+
+        Role::findByName('customer_service', 'web')->givePermissionTo($managePermissions);
+        Role::findByName('super_admin', 'web')->givePermissionTo($managePermissions);
     }
 }
